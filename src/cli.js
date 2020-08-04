32ef382da5af68486289ea4806a96094dd8edaed
KalyakinAG <Andrey.Kalyakin@gmail.com>
2020-08-04 22:55:58 +0300

    Подключена библиотека readline-sync

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+    const userName = readlineSync.question('Как вас зовут? ');
+    console.log('Привет, ' + userName + '!');
+};
