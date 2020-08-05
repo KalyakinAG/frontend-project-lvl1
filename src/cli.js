9f7ade10dc9992be670e15634b3fc780ed9c4121
KalyakinAG <Andrey.Kalyakin@gmail.com>
2020-08-05 23:22:41 +0300

    Изменен код для отработки замечания проверки eslint

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,6 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   const userName = readlineSync.question('Как вас зовут? ');
+  const temp = '';
   console.log(`Привет, ${userName}!`);
 };
