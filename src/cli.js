dc9e2d0d599a1598dfa4d504c7939243543ad969
KalyakinAG <Andrey.Kalyakin@gmail.com>
2020-08-07 23:12:03 +0300

    Устранение замечаний lint

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   const userName = readlineSync.question('Как вас зовут? ');
-  const temp = '';
   console.log(`Привет, ${userName}!`);
 };
